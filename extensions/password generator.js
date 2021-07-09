8a9dbcae130916f94dfb3eeacf087f034388c5d7
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-07-09 07:56:49 +0300

    Create password generator.js

---
+++ extensions/password generator.js
@@ -0,0 +1,11 @@
+function generatePassword(symbols) {
+  var length = symbols,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+prompt("", generatePassword(10));
