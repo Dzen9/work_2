729db7f5b3f5756b82d63cc6331e04926ec16009
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-07-11 22:22:52 +0300

    Create to_put_element_on_page.js

---
+++ extensions/to_put_element_on_page.js
@@ -0,0 +1,6 @@
+// created button
+var element = document
+  .createElement("button")
+  .appendChild(document.createTextNode("Click Me!"));
+var page = document.getElementById("btn");
+page.appendChild(element);
