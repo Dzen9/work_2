5b1c98bdcb2e37c4dc9fb9b1d5677a9462a0ee77
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-07-16 07:44:49 +0300

    Update find pavel and marina list watchers.js

--- new ticket project/find pavel and marina list watchers.js
+++ new ticket project/find pavel and marina list watchers.js
@@ -1,4 +1,3 @@
 // find marina and pasha into наблюдатели (найти наблюдателей)
 let inputs = document.getElementsByName( "watcher[user_ids][]" );
 inputs.forEach(element=>{if(element.value === "355" || element.value === "237") {element.checked = true}});
-undefined
