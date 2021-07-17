43a8f035190aef4d7bc179e03e1eb74fb5ef2563
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-07-17 13:19:00 +0300

    Update okpd.js

--- support/okpd.js
+++ support/okpd.js
@@ -4,8 +4,15 @@ var btn = document.createElement("BUTTON"); // Create a <button> element
 btn.innerHTML = "OKPD"; // Insert text
 btn.addEventListener("click", example);
 // document.body.appendChild(btn); // Append <button> to <body>
-var setTo = document.getElementsByClassName("ticket-tobpar-actions__common-actions")[0];
+var setTo = document.getElementsByClassName(
+  "ticket-tobpar-actions__common-actions"
+)[0];
 setTo.appendChild(btn);
 function example() {
-  window.open("https://yandex.ru/search/?lr=10668&text=" + window.getSelection().baseNode.data);
+  let str = window.getSelection().baseNode.data;
+  let open = "https://yandex.ru/search/?lr=10668&text=";
+  if (str == undefined || str.match(/.*\d{19}.*/) == null) {
+      str = "";
+  }
+  window.open(open + str);
 }
