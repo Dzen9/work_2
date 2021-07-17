b589d290b8eb633bdc24e0dfb4f4529d2d20f5c0
Dzen9 <86401277+Dzen9@users.noreply.github.com>
2021-07-17 15:26:51 +0300

    Update okpd.js

--- support/okpd.js
+++ support/okpd.js
@@ -2,15 +2,15 @@
 // change path
 var btn = document.createElement("BUTTON"); // Create a <button> element
 btn.innerHTML = "OKPD"; // Insert text
-btn.addEventListener("click", example);
+btn.addEventListener("click", fnFindOKPD);
 // document.body.appendChild(btn); // Append <button> to <body>
 var setTo = document.getElementsByClassName(
   "ticket-tobpar-actions__common-actions"
 )[0];
 setTo.appendChild(btn);
-function example() {
-  var open = "https://yandex.ru/search/?lr=10668&text=";
-  var str = "";
+function fnFindOKPD() {
+  let open = "https://yandex.ru/search/?lr=10668&text=";
+  let str = "";
   if (window.getSelection().baseNode == undefined) {
     str = "";
   } else {
@@ -23,6 +23,59 @@ function example() {
   window.open(open + str);
 }
 //=================================================================
+// TO FIND TOPIC IN SEARCH
+var btn = document.createElement("BUTTON"); // Create a <button> element
+btn.innerHTML = "SCH PR D"; // Insert text
+btn.addEventListener("click", fnFindProjInSearchDescription);
+setTo.appendChild(btn);
+
+function fnFindProjInSearchDescription() {
+  let value = prompt("Please input topic for serching", "");
+
+  if (value != undefined) {
+    window.open(
+      "https://projects.srvhub.tools/issues?utf8=%E2%9C%93&set_filter=1&sort=updated_on%3Adesc%2Cstart_date%3Adesc%2Cid%3Adesc&f%5B%5D=subject&op%5Bsubject%5D=*&f%5B%5D=description&op%5Bdescription%5D=%7E&v%5Bdescription%5D%5B%5D=" +
+        encodeURI(value) +
+        "&f%5B%5D=&c%5B%5D=project&c%5B%5D=tracker&c%5B%5D=cf_14&c%5B%5D=subject&c%5B%5D=author&c%5B%5D=assigned_to&c%5B%5D=start_date&c%5B%5D=updated_on&c%5B%5D=status&c%5B%5D=cf_15&group_by=&t%5B%5D="
+    );
+  }
+}
+//=================================================================
+var btn = document.createElement("BUTTON"); // Create a <button> element
+btn.innerHTML = "SCH PR T"; // Insert text
+btn.addEventListener("click", fnFindProjInSearchTopic);
+setTo.appendChild(btn);
+
+function fnFindProjInSearchTopic() {
+  let value = prompt("Please input topic for serching", "");
+
+  if (value != undefined) {
+    window.open(
+      "https://projects.srvhub.tools/issues?utf8=%E2%9C%93&set_filter=1&sort=updated_on%3Adesc%2Cstart_date%3Adesc%2Cid%3Adesc&f%5B%5D=subject&op%5Bsubject%5D=%7E&v%5Bsubject%5D%5B%5D=" +
+        encodeURI(value) +
+        "&f%5B%5D=description&op%5Bdescription%5D=*&f%5B%5D=&c%5B%5D=project&c%5B%5D=tracker&c%5B%5D=cf_14&c%5B%5D=subject&c%5B%5D=author&c%5B%5D=assigned_to&c%5B%5D=start_date&c%5B%5D=updated_on&c%5B%5D=status&c%5B%5D=cf_15&group_by=&t%5B%5D="
+    );
+  }
+}
+
+// ================================================================
+var btn = document.createElement("BUTTON"); // Create a <button> element
+btn.innerHTML = "SCH SUP"; // Insert text
+btn.addEventListener("click", fnFindTopicInSupport);
+setTo.appendChild(btn);
+
+function fnFindTopicInSupport() {
+  let value = prompt("Please input topic for serching", "");
+
+  if (value != undefined) {
+    window.open(
+      "https://support.srvhub.ru/ru/ticket/list/filter/id/search/page/1?search=" +
+        encodeURI(value)
+    );
+  }
+}
+
+// ================================================================
 // // FAST GO TO WRITE TEXT
 // var getIdEditor = document.getElementsByClassName(
 //   "ck-blurred ck ck-content ck-editor__editable ck-rounded-corners ck-editor__editable_inline"
